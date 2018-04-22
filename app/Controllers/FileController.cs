7b3bf8c80b0c4ef963d077daf1817828836cdcd0
lcbigdaddy <12125998@qq.com>
2018-04-22 12:08:55 +0800

    删除无用

---
+++ app/Controllers/FileController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace app.Controllers
+{
+    public class FileController : Controller
+    {
+        //
+        // GET: /File/
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var map = new Dictionary<string, string>();
+            map.Add("C#", "0");
+            map.Add("C++", "1");
+            map.Add("C", "2");
+            map.Add("VB", "2"); 
+            return Json(new { Result = 0, Msg = "成功", data = map }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
