498f9188ccfd6e03dcdd3b82fffee3995453554b
lcbigdaddy <12125998@qq.com>
2018-04-23 12:07:03 +0800

    文件展示接口提供

--- app/Controllers/FileController.cs
+++ app/Controllers/FileController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.IO;
 
 namespace app.Controllers
 {
@@ -10,15 +11,21 @@ namespace app.Controllers
     {
         //
         // GET: /File/
+        [AllowCrossSiteJson]
         [HttpGet]
         public ActionResult Index()
         {
+            string root_path = Path.Combine(Server.MapPath(""), "ShareFiles");
+            DirectoryInfo dir = new DirectoryInfo(root_path);
+            List<Dictionary<string, string>> arr = new List<Dictionary<string, string>>();
             var map = new Dictionary<string, string>();
-            map.Add("C#", "0");
-            map.Add("C++", "1");
-            map.Add("C", "2");
-            map.Add("VB", "2"); 
-            return Json(new { Result = 0, Msg = "成功", data = map }, JsonRequestBehavior.AllowGet);
+            foreach (FileInfo d in dir.GetFiles())
+            {
+                map.Add("Name",d.Name);
+                map.Add("FullName", d.FullName.Replace("\\","/"));
+                arr.Add(map);
+            }
+            return Json(new { Result = 0, Msg = "成功", data = arr }, JsonRequestBehavior.AllowGet);
         }
     }
 }
